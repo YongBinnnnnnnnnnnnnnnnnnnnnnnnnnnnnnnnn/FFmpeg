eb776efba6da0c6fefaaed2853e6e6a696f0c914
twinaphex <libretro@gmail.com>
2014-05-04 05:49:45 +0200

    (Libretro) BAKE_IN_FFMPEG bakes in libavutil now as well

--- libretro/config.h
+++ libretro/config.h
@@ -1,2 +1,14 @@
 #define FFMPEG_CONFIGURATION ""
 #define FFMPEG_LICENSE ""
+#define CONFIG_MEMORY_POISONING 0
+#define CONFIG_FTRAPV 0
+
+#ifndef FF_MEMORY_POISON
+#define FF_MEMORY_POISON 0x2a
+#endif
+
+#ifndef MAKE_ACCESSORS
+#define MAKE_ACCESSORS(str, name, type, field) \
+    type av_##name##_get_##field(const str *s) { return s->field; } \
+    void av_##name##_set_##field(str *s, type v) { s->field = v; }
+#endif
