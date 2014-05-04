77b68062c9b1df0a503760b9d605295f48ff3ae8
twinaphex <libretro@gmail.com>
2014-05-04 16:53:56 +0200

    (Libretro) Bake in libavformat now too if BAKE_IN_FFMPEG is defined

--- libretro/config.h
+++ libretro/config.h
@@ -2,6 +2,8 @@
 #define FFMPEG_LICENSE ""
 #define CONFIG_MEMORY_POISONING 0
 #define CONFIG_FTRAPV 0
+#define av_restrict restrict
+#define CONFIG_LIBMP3LAME 1
 
 #ifndef FF_MEMORY_POISON
 #define FF_MEMORY_POISON 0x2a
