1a793fa2f4b6630e0b0ed7a7751a9abd4c34bcf5
Bin Yong <binaryy@163.com>
2024-10-18 02:25:19 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,12 +2,12 @@
 
 
 export LDFLAGS="-EL -nostdlib -z max-page-size=32"
-export CXX_LDFLAGS="-EL -mtune=mips32 -msoft-float -Wl,--gc-sections --static -z max-page-size=32"
+export CXX_LDFLAGS="-EL -Wl,--gc-sections --static -z max-page-size=32"
 
-export CFLAGS="-EL -mtune=mips32 -msoft-float -Os -G0 -mno-abicalls -fno-pic -ffunction-sections -fdata-sections -I ../../libs/libretro-common/include"
+export CFLAGS="-EL -Os -G0 -mno-abicalls -fno-pic -ffunction-sections -fdata-sections -I ../../libs/libretro-common/include"
 export PATH=/opt/mips32-mti-elf/2019.09-03-2/bin/:$PATH
 export LD_LIBRARY_PATH=/opt/mips32-mti-elf/2019.09-03-2/lib 
-./configure --arch=mips32 --target-os=none --cross-prefix=mips-mti-elf- --disable-asm
+./configure --arch=mips --target-os=none --cross-prefix=mips-mti-elf- --disable-asm
 sed -i config.h -e "s|#define getenv(x) NULL|inline char* getenv(const char *a){return (char*)0;}|" -e "s|HAVE_CBRT 0|HAVE_CBRT 1|" -e "s|HAVE_RINT 0|HAVE_RINT 1|" -e "s|HAVE_LRINT 0|HAVE_LRINT 1|" -e "s|HAVE_LRINTF 0|HAVE_LRINTF 1|" -e "s|HAVE_ROUND 0|HAVE_ROUND 1|" -e "s|HAVE_TRUNC 0|HAVE_TRUNC 1|" -e "s|HAVE_ISINF 0|HAVE_ISINF 1|" -e "s|HAVE_ISNAN 0|HAVE_ISNAN 1|"
 
 
