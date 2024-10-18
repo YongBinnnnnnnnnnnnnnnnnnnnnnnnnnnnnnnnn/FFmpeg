948e2280355a06ee7452e32f05bf60d8a727a3f5
Bin Yong <binaryy@163.com>
2024-10-18 01:26:57 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,6 +2,8 @@
 
 PATH=/opt/mips32-mti-elf/2019.09-03-2/bin/:$PATH ./configure --arch=mips --target-os=none --cross-prefix=mips-mti-elf-
 sed -i config.h -e "s|#define getenv(x) NULL|inline char* getenv(const char *a){return (char*)0;}|"
+sed -i libavutil/time_internal.h -e "s|static inline|inline|g"
+
 make
 
 
