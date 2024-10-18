4886cc179a2ca92fc04aeaef58a19b3962aeb016
Bin Yong <binaryy@163.com>
2024-10-18 01:24:29 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 PATH=/opt/mips32-mti-elf/2019.09-03-2/bin/:$PATH ./configure --arch=mips --target-os=none --cross-prefix=mips-mti-elf-
+sed -i config.h -e "s|#define getenv(x) NULL|inline char* getenv(const char *a){return (char*)0;}|"
 make
+
+
